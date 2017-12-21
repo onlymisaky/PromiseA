d98c63d5688eff217b9ea24846eab42e0954964e
zsq <onlymisaky@gmail.com>
2017-12-21 10:18:00 +0800

    add fs-promise

---
+++ fs.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+const fsPromise = (...args) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile.call(null, ...args, (err, data) => {
+            return err ? reject(err) : resolve(data);
+        });
+    });
+}

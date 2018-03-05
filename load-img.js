2bfa236207331f76621885f9c6660ada15c6225d
zsq <onlymisaky@gmail.com>
2018-03-05 11:36:43 +0800

    add load-img

---
+++ load-img.js
@@ -0,0 +1,14 @@
+export const loadImg = url => new Promise((resolve, reject) => {
+    const img = new Image();
+    img.onload = () => resolve(img);
+    img.onerror = (e) => reject(e);
+    img.crossOrigin = 'Anonymous';
+    img.src = url;
+});
+
+export const readFile = (blob, readAs, encoding) => new Promise((resolve, reject) => {
+    const fr = new FileReader();
+    fr.onload = () => resolve(fr.result);
+    fr.onerror = () => resolve(fr.error);
+    fr[readAs](blob, encoding);
+});
